c2852c04278f7117d4e98ad9e693e024674bd8d4
elucidium <xun.ruiran@gmail.com>
2020-03-07 22:12:28 -0500

    Better layout and integrated user local time

--- map.js
+++ map.js
@@ -21,9 +21,16 @@ require([
 
   });
 
+
+function onLoad() {
+    document.getElementById("greeting").innerHTML = "<div><p class='lead'>Hello!</p></div><div><p>Please enable location services to continue.</p></div>";
+    getUserLocation();
+}
+
 function getUserLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(passPosition);
+        
     }
 }
 
@@ -31,11 +38,19 @@ function passPosition(position) {
     userLongitude = position.coords.longitude;
     userLatitude = position.coords.latitude;
     view.goTo([userLongitude, userLatitude]);
+    var now = new Date();
+    var time = '';
+    var hours = now.getHours();
+    if (4 <= hours && hours < 12) time = 'morning';
+    else if (12 <= hours && hours < 18) time = 'afternoon';
+    else time = 'evening';
+    document.getElementById("greeting").innerHTML = "<div><p class='lead'>Good " + time + "! What do you want to eat?</p></div>";
 }
 
 function search() {
     var request = new XMLHttpRequest();
-    request.open('GET', 'placeholder', true);
+    var term = ''; // TODO fix this
+    request.open('GET', 'https://api.yelp.com/v3/businesses/search?latitude='+userLatitude+'&longitude='+userLongitude+'&term='+term, true);
     request.onload = function() {
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
