e7308db90f3edcadb9092c4e805dfff250f176e8
elucidium <xun.ruiran@gmail.com>
2020-03-08 20:40:16 -0400

    Final working product with proper commented documentation

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+// Variable declarations used throughout the app.
+
 var map;
 var view;
 var pointsLayer;
@@ -6,81 +8,30 @@ var userLatitude = 0;
 var userLongitude = 0;
 var data;
 
-// Prevent form from refreshing the page upon pressing 'enter', then initiate search.
-$("form").submit(function (e) {
-    e.preventDefault();
-    console.log("Search initiated");
-    search();
-});
-
 // Initialize the ArcGIS map.
 require([
     "esri/Map",
-    "esri/views/MapView",
-    "esri/layers/GraphicsLayer"
-  ], function(Map, MapView, GraphicsLayer) {
+    "esri/views/MapView"
+  ], function(Map, MapView) {
     map = new Map({
       basemap: "topo-vector"
     });
-
     view = new MapView({
       container: "map-view",
       map: map,
       center: [userLongitude, userLatitude],
       zoom: 15
     });
-    pointsLayer = new GraphicsLayer({
-        graphics: pointsArray
-    });
-    map.add(pointsLayer);
-/*
-    map.on("load", function() {
-        var gl = new GraphicsLayer();
-        data.forEach(business => {
-            var point = new Pointer(business.coordinates.longitude, business.coordinates.latitude);
-            var symbol = new SimpleMarkerSymbol().setSize(30);
-            var pointGraphic = new Graphic(p, s);
-            gl.add(pointGraphic);
-        })
-        map.addLayer(gl);
-    });*/
-    /*if (typeof data != "undefined") {
-        data.forEach(business => {
-            console.log("Trying to make point");
-            var point = {
-                type: "point",
-                longitude: business.coordinates.longitude,
-                latitude: business.coordinates.latitude
-            };
-            var symbol = {
-                type: "simple-marker",
-                color: [226, 119, 40], // CHANGE TO LOGO COLOR LATER
-                outline: {
-                    color: [255, 255, 255],
-                    width: 2
-                }
-            };
-            var pointGraphic = new pointGraphic({
-                geometry: point,
-                symbol: symbol
-            });
-            
-            view.graphics.add(pointGraphic);
-        });
-    }*/
-
   });
 
 // Upon page loading, prompt user to enable location services, then get the user's location.
 function onLoad() {
-    console.log("Console test");
     document.getElementById("greeting").innerHTML = "<div><p class='lead'>Hello! Nice to meet you.</p></div><div><p>Please enable location services to continue.</p></div>";
     getUserLocation();
 }
 
 // Get the user's location.
 function getUserLocation() {
-    console.log("ran GetUserLocation");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(passPosition);        
     }
@@ -88,7 +39,6 @@ function getUserLocation() {
 
 // Function that handles the user location and then updates the website's user prompt.
 function passPosition(position) {
-    console.log("ran passPosition");
     userLongitude = position.coords.longitude;
     userLatitude = position.coords.latitude;
     view.goTo([userLongitude, userLatitude]);
@@ -99,68 +49,4 @@ function passPosition(position) {
     else if (12 <= hours && hours < 18) time = 'afternoon';
     else time = 'evening';
     document.getElementById("greeting").innerHTML = "<div><p class='lead'>Good " + time + "! What would you like to eat?</p></div>";
-}
-
-function search() {
-    console.log("Using search");
-    'use strict';
-    document.getElementById('output').innerHTML = '<p style="text-align:center">Searching Yelp...</p>';
-    var request = new XMLHttpRequest();
-    var term = document.getElementById('search').value; // TODO fix this
-    request.open('GET', 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude='+userLatitude+'&longitude='+userLongitude+'&term='+term, true);
-    request.setRequestHeader("Authorization", "Bearer te408GrT8_MYTERNfuzhCx7tg7Z_eYjgtctRUhuUf_64G0eMygUST-N-1zqPCa-OB8thDcwKdH1EUhXsIlOEU7q0geAw64gCPNp-p-8_mkpiVwLXS6bCLrfqVgVkXnYx");
-    request.onload = function() {
-        data = JSON.parse(this.response).businesses;
-        if (request.status >= 200 && request.status < 400) {
-            console.log("Made it through the call!");
-            //document.getElementById('output').innerHTML = (JSON.stringify(data));
-            document.getElementById('output').innerHTML = '';
-            data.forEach(business => {
-                //document.getElementById('output').append(business.name);
-                const card = document.createElement('div');
-                card.setAttribute('class', 'card');
-                //  link="#861c5e" vlink="#a82475" alink="#bd2884"
-                const h1 = document.createElement('h1');
-                h1.innerHTML = '<a href="' + business.url + '" target="blank" rel="noopener noreferrer">' + business.name + '</a>';
-                h1.setAttribute('class', 'logo-purple');
-                const p1 = document.createElement('p');
-                
-                var price = business.price;
-                if (typeof price == "undefined") price = "unspecified";
-                var distance = business.distance;
-                distance = (distance / 1000).toFixed(2);
-                p1.textContent = 'rating: ' + business.rating + "/5 | price range: " + price + " | distance: " + distance + " km";
-                card.appendChild(h1);
-                card.appendChild(p1);
-                document.getElementById('output').appendChild(card);
-            });
-            require(["esri/layers/GraphicsLayer", "esri/Graphic"], function(GraphicsLayer, Graphic) {
-                var layer = new GraphicsLayer();
-                data.forEach(business => {
-                    var point = {
-                        type: "point",
-                        longitude: business.coordinates.longitude,
-                        latitude: business.coordinates.latitude
-                    };
-                    var symbol = {
-                        type: "simple-marker",
-                        color: [35, 102, 69],
-                        outline: {
-                            color: [255, 255, 255],
-                            width: 2
-                        }
-                    };
-                    var pointGraphic = new Graphic({
-                        geometry: point,
-                        symbol: symbol
-                    });
-                    layer.add(pointGraphic);
-                })
-                map.add(layer);
-            })
-        } else {
-            alert("Error occurred while searching Yelp.")
-        }
-    }
-    request.send();
 }
--- search.js
+++ search.js
@@ -1,62 +1,95 @@
-// deprecated code; moved into map.js
+// Prevent form from refreshing the page upon pressing 'enter', then initiate search.
 $("form").submit(function (e) {
     e.preventDefault();
-    console.log("Search initiated");
     search();
 });
 
+// Executed every time the form is submitted (i.e. 'enter' is pressed).
 function search() {
-    console.log("Using search");
     'use strict';
-    document.getElementById('output').innerHTML = '';
+    // Prompt while search results are loading
+    document.getElementById('output').innerHTML = '<p style="text-align:center">Searching Yelp...</p>';
+    // JSON request
     var request = new XMLHttpRequest();
-    var term = document.getElementById('search').value; // TODO fix this
+    var term = document.getElementById('search').value;
     request.open('GET', 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude='+userLatitude+'&longitude='+userLongitude+'&term='+term, true);
     request.setRequestHeader("Authorization", "Bearer te408GrT8_MYTERNfuzhCx7tg7Z_eYjgtctRUhuUf_64G0eMygUST-N-1zqPCa-OB8thDcwKdH1EUhXsIlOEU7q0geAw64gCPNp-p-8_mkpiVwLXS6bCLrfqVgVkXnYx");
+    // Response to JSON request
     request.onload = function() {
         data = JSON.parse(this.response).businesses;
+        // Runs if the request was successful.
         if (request.status >= 200 && request.status < 400) {
-            console.log("Made it through the call!");
-            //document.getElementById('output').innerHTML = (JSON.stringify(data));
+            document.getElementById('output').innerHTML = '<p style="text-align:center">Click on the restaurants in the map to learn more.</p><p style="text-align:center"><em>Not feeling it? Try another search!</em></p>';
+            // For each business, make a card with basic information and append to output.
             data.forEach(business => {
-                //document.getElementById('output').append(business.name);
                 const card = document.createElement('div');
                 card.setAttribute('class', 'card');
                 const h1 = document.createElement('h1');
-                h1.textContent = business.name;
+                h1.innerHTML = '<a href="' + business.url + '" target="blank" rel="noopener noreferrer">' + business.name + '</a>';
                 h1.setAttribute('class', 'logo-purple');
                 const p1 = document.createElement('p');
-                p1.textContent = 'Rating: ' + business.rating;
-                const p2 = document.createElement('p');
-                p2.textContent = 'Price: ' + business.price;
+                var price = business.price;
+                if (typeof price == "undefined") price = "unspecified";
+                var distance = business.distance;
+                distance = (distance / 1000).toFixed(2);
+                p1.textContent = 'rating: ' + business.rating + "/5 | price range: " + price + " | distance: " + distance + " km";
                 card.appendChild(h1);
                 card.appendChild(p1);
-                card.appendChild(p2);
                 document.getElementById('output').appendChild(card);
-                /*var point = {
-                    type: "point",
-                    longitude: business.coordinates.longitude,
-                    latitude: business.coordinates.latitude
-                };
-                var symbol = {
-                    type: "simple-marker",
-                    color: [226, 119, 40], // CHANGE TO LOGO COLOR LATER
-                    outline: {
-                        color: [255, 255, 255],
-                        width: 2
-                    }
-                };
-                var pointGraphic = new esri.pointGraphic({
-                    geometry: point,
-                    symbol: symbol
-                });
-                
-                map.graphics.add(pointGraphic);*/
             });
+            document.getElementById('output').append();
+            // For each business, add a point to the ArcGIS map.
+            require(["esri/layers/GraphicsLayer", "esri/Graphic"], function(GraphicsLayer, Graphic) {
+                var pointsLayer = new GraphicsLayer();
+                data.forEach(business => {
+                    var point = {
+                        type: "point",
+                        longitude: business.coordinates.longitude,
+                        latitude: business.coordinates.latitude
+                    };
+                    var symbol = {
+                        type: "simple-marker",
+                        color: [35, 102, 69],
+                        outline: {
+                            color: [255, 255, 255],
+                            width: 2
+                        }
+                    };
+                    var information = {
+                        Name: business.name,
+                        Category: business.categories.title,
+                        Address: business.location.display_address,
+                        Phone: business.display_phone,
+                        Options: business.transactions
+                    }
+                    var pointGraphic = new Graphic({
+                        geometry: point,
+                        symbol: symbol,
+                        attributes: information,
+                        popupTemplate: {
+                            title: "{Name}",
+                            content: [
+                              {
+                                type: "fields",
+                                fieldInfos: [
+                                  { fieldName: "Name" },
+                                  { fieldName: "Category" },
+                                  { fieldName: "Address" },
+                                  { fieldName: "Phone" },
+                                  { fieldName: "Options" }
+                                ]
+                              }
+                            ]
+                          }
+                    });
+                    pointsLayer.add(pointGraphic);
+                })
+                map.removeAll();
+                map.add(pointsLayer);
+            })
         } else {
             alert("Error occurred while searching Yelp.")
         }
     }
-    
     request.send();
 }
